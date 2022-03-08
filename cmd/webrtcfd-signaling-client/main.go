ae3490f778b056572f33774ce9cd35cd694a97e2
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-03-08 23:42:40 +0100

    feat: Add password authentication to signaling server

--- cmd/webrtcfd-signaling-client/main.go
+++ cmd/webrtcfd-signaling-client/main.go
@@ -20,6 +20,7 @@ import (
 var (
 	errMissingCommunityID = errors.New("missing community ID")
 	errMissingPassword    = errors.New("missing password")
+	errMissingKey         = errors.New("missing encryption key")
 )
 
 func main() {
@@ -27,6 +28,7 @@ func main() {
 	timeout := flag.Duration("timeout", time.Second*10, "Time to wait for connections")
 	communityID := flag.String("community", "", "ID of community to join")
 	password := flag.String("password", "", "Password for community")
+	key := flag.String("key", "", "Encryption key for community")
 	verbose := flag.Bool("verbose", false, "Enable verbose logging")
 
 	flag.Parse()
@@ -39,13 +41,22 @@ func main() {
 		panic(errMissingPassword)
 	}
 
+	if strings.TrimSpace(*key) == "" {
+		panic(errMissingKey)
+	}
+
 	log.Println("Connecting to signaler with address", *raddr)
 
 	u, err := url.Parse(*raddr)
 	if err != nil {
 		panic(err)
 	}
+
 	u.Path = path.Join(u.Path, *communityID)
+
+	q := u.Query()
+	q.Add("password", *password)
+	u.RawQuery = q.Encode()
 
 	lines := make(chan []byte)
 	go func() {
@@ -60,7 +71,7 @@ func main() {
 		func() {
 			defer func() {
 				if err := recover(); err != nil {
-					log.Println("closed connection to signaler with address", *raddr+":", err)
+					log.Println("closed connection to signaler with address", *raddr+":", err, "(wrong password?)")
 				}
 
 				log.Println("Reconnecting to signaler with address", *raddr, "in", *timeout)
@@ -111,7 +122,7 @@ func main() {
 				case err := <-errs:
 					panic(err)
 				case input := <-inputs:
-					input, err = encryption.Decrypt(input, []byte(*password))
+					input, err = encryption.Decrypt(input, []byte(*key))
 					if err != nil {
 						if *verbose {
 							log.Println("Could not decrypt message with length", len(input), "for signaler with address", conn.RemoteAddr(), "in community", *communityID+", skipping")
@@ -126,7 +137,7 @@ func main() {
 
 					fmt.Printf("%s\n", input)
 				case line := <-lines:
-					line, err = encryption.Encrypt(line, []byte(*password))
+					line, err = encryption.Encrypt(line, []byte(*key))
 					if err != nil {
 						panic(err)
 					}
