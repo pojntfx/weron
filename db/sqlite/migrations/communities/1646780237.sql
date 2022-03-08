1c929afe62260ae7cbbe9f3bb265d142b4b58ab8
Felix Pojtinger <felix@pojtinger.com>
2022-03-09 00:49:11 +0100

    feat: Add `persistent` DB flag

--- db/sqlite/migrations/communities/1646780237.sql
+++ db/sqlite/migrations/communities/1646780237.sql
@@ -1,7 +1,8 @@
 -- +migrate Up
 create table communities (
     id text primary key not null,
-    password text not null
+    password text not null,
+    persistent bool not null
 );
 -- +migrate Down
 drop table communities;
