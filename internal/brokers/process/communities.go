8b6f9e2a938b9d056f25f8688eef27f20c23f687
Felix Pojtinger <felix@pojtinger.com>
2022-03-27 22:45:51 +0200

    fix: `Notify` instead of `Broadcast` on local broker

--- internal/brokers/process/communities.go
+++ internal/brokers/process/communities.go
@@ -32,7 +32,7 @@ func (c *CommunitiesBroker) Open(ctx context.Context, brokerURL string) error {
 func (c *CommunitiesBroker) SubscribeToKicks(ctx context.Context, errs chan error) (chan brokers.Kick, func() error) {
 	kicks := make(chan brokers.Kick)
 
-	l := c.kicks.Listener(1)
+	l := c.kicks.Listener(0)
 	rawKicks := l.Ch()
 
 	go func() {
@@ -56,7 +56,7 @@ func (c *CommunitiesBroker) SubscribeToKicks(ctx context.Context, errs chan erro
 func (c *CommunitiesBroker) SubscribeToInputs(ctx context.Context, errs chan error, community string) (chan brokers.Input, func() error) {
 	inputs := make(chan brokers.Input)
 
-	l := c.inputs.Listener(1)
+	l := c.inputs.Listener(0)
 	rawInputs := l.Ch()
 
 	go func() {
@@ -78,13 +78,13 @@ func (c *CommunitiesBroker) SubscribeToInputs(ctx context.Context, errs chan err
 }
 
 func (c *CommunitiesBroker) PublishInput(ctx context.Context, input brokers.Input, community string) error {
-	c.inputs.Broadcast(input)
+	c.inputs.NotifyCtx(ctx, input)
 
 	return nil
 }
 
 func (c *CommunitiesBroker) PublishKick(ctx context.Context, kick brokers.Kick) error {
-	c.kicks.Broadcast(kick)
+	c.kicks.NotifyCtx(ctx, kick)
 
 	return nil
 }
