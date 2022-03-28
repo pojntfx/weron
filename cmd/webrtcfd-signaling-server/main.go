036d32d628a993e0db76d422810ac0db97b546fc
Felix Pojtinger <felix@pojtinger.com>
2022-03-28 21:17:27 +0200

    refactor: Decompose signaling server into `wrtcsgl`

--- cmd/webrtcfd-signaling-server/main.go
+++ cmd/webrtcfd-signaling-server/main.go
@@ -2,34 +2,18 @@ package main
 
 import (
 	"context"
-	"database/sql"
-	"encoding/json"
 	"errors"
 	"flag"
-	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
-	"strings"
-	"sync"
 	"syscall"
 	"time"
 
-	rediserr "github.com/go-redis/redis/v8"
-	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
-	"github.com/pojntfx/webrtcfd/internal/authn"
-	"github.com/pojntfx/webrtcfd/internal/authn/basic"
-	"github.com/pojntfx/webrtcfd/internal/authn/oidc"
-	"github.com/pojntfx/webrtcfd/internal/brokers"
-	"github.com/pojntfx/webrtcfd/internal/brokers/process"
-	"github.com/pojntfx/webrtcfd/internal/brokers/redis"
-	"github.com/pojntfx/webrtcfd/internal/persisters"
-	"github.com/pojntfx/webrtcfd/internal/persisters/memory"
-	"github.com/pojntfx/webrtcfd/internal/persisters/psql"
+	"github.com/pojntfx/webrtcfd/pkg/wrtcsgl"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
@@ -133,58 +117,32 @@ func main() {
 		*oidcClientID = u
 	}
 
-	managementAPIEnabled := true
-	if (strings.TrimSpace(*oidcIssuer) == "" && strings.TrimSpace(*oidcClientID) == "") && strings.TrimSpace(*apiPassword) == "" {
-		managementAPIEnabled = false
+	signaler := wrtcsgl.NewSignaler(
+		*laddr,
+		*postgresURL,
+		*redisURL,
+		&wrtcsgl.SignalerConfig{
+			Heartbeat:           *heartbeat,
+			Cleanup:             *cleanup,
+			EphermalCommunities: *ephermalCommunities,
+			APIUsername:         *apiUsername,
+			APIPassword:         *apiPassword,
+			OIDCIssuer:          *oidcIssuer,
+			OIDCClientID:        *oidcClientID,
+			Verbose:             *verbose,
+			OnConnect: func(raddr, community string) {
+				log.Println("Connected to client with address", raddr, "in community", community)
+			},
+			OnDisconnect: func(raddr, community string, err interface{}) {
+				log.Println("Disconnected from client with address", raddr, "in community", community)
+			},
+		},
+		ctx,
+	)
 
-		if *verbose {
-			log.Println("API password not set, disabling management API")
-		}
-	}
-
-	var db persisters.CommunitiesPersister
-	if strings.TrimSpace(*postgresURL) == "" {
-		db = memory.NewCommunitiesPersister()
-	} else {
-		db = psql.NewCommunitiesPersister()
-	}
-
-	if err := db.Open(*postgresURL); err != nil {
+	if err := signaler.Open(); err != nil {
 		panic(err)
 	}
-
-	if *cleanup {
-		if err := db.Cleanup(ctx); err != nil {
-			panic(err)
-		}
-	}
-
-	var broker brokers.CommunitiesBroker
-	if strings.TrimSpace(*redisURL) == "" {
-		broker = process.NewCommunitiesBroker()
-	} else {
-		broker = redis.NewCommunitiesBroker()
-	}
-
-	if err := broker.Open(ctx, *redisURL); err != nil {
-		panic(err)
-	}
-
-	var authn authn.Authn
-	if strings.TrimSpace(*oidcIssuer) == "" && strings.TrimSpace(*oidcClientID) == "" {
-		authn = basic.NewAuthn(*apiUsername, *apiPassword)
-	} else {
-		authn = oidc.NewAuthn(*oidcIssuer, *oidcClientID)
-	}
-
-	if err := authn.Open(ctx); err != nil {
-		panic(err)
-	}
-
-	srv := &http.Server{Addr: addr.String()}
-
-	var connectionsLock sync.Mutex
-	connections := map[string]map[string]connection{}
 
 	s := make(chan os.Signal)
 	signal.Notify(s, os.Interrupt, syscall.SIGTERM)
@@ -193,8 +151,6 @@ func main() {
 
 		log.Println("Gracefully shutting down signaling server")
 
-		s := make(chan os.Signal)
-		signal.Notify(s, os.Interrupt, syscall.SIGTERM)
 		go func() {
 			<-s
 
@@ -202,366 +158,27 @@ func main() {
 
 			cancel()
 
-			if err := broker.Close(); err != nil {
-				if err != context.Canceled && err != rediserr.ErrClosed {
-					panic(err)
-				}
-			}
-
-			if err := srv.Shutdown(ctx); err != nil {
-				if err != context.Canceled {
-					panic(err)
-				}
+			if err := signaler.Close(); err != nil {
+				panic(err)
 			}
 		}()
 
-		connectionsLock.Lock()
-		defer connectionsLock.Unlock()
-		for c := range connections {
-			for range connections[c] {
-				if err := db.RemoveClientFromCommunity(ctx, c); err != nil {
-					panic(err)
-				}
-			}
+		if err := signaler.Close(); err != nil {
+			panic(err)
 		}
 
 		cancel()
-
-		if err := broker.Close(); err != nil {
-			if err != context.Canceled && err != rediserr.ErrClosed {
-				panic(err)
-			}
-		}
-
-		if err := srv.Shutdown(ctx); err != nil {
-			if err != context.Canceled {
-				panic(err)
-			}
-		}
 	}()
 
-	errs := make(chan error)
-	kicks, closeKicks := broker.SubscribeToKicks(ctx, errs)
 	defer func() {
-		if err := closeKicks(); err != nil {
+		if err := signaler.Close(); err != nil {
 			panic(err)
-		}
-	}()
-
-	srv.Handler = http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		raddr := uuid.New().String()
-
-		defer func() {
-			err := recover()
-			switch err {
-			case nil:
-				log.Println("closed connection for client with address", raddr)
-			case http.StatusUnauthorized:
-				fallthrough
-			case http.StatusNotImplemented:
-				log.Println("closed connection for client with address", raddr+":", err)
-			case http.StatusNotFound:
-				log.Println("closed connection for client with address", raddr+":", err)
-			default:
-				rw.WriteHeader(http.StatusInternalServerError)
-
-				log.Println("closed connection for client with address", raddr+":", err)
-			}
-		}()
-
-		switch r.Method {
-		case http.MethodGet:
-			community := r.URL.Query().Get("community")
-			if strings.TrimSpace(community) == "" {
-				if !managementAPIEnabled {
-					rw.WriteHeader(http.StatusNotImplemented)
-
-					panic(http.StatusNotImplemented)
-				}
-
-				// List communities
-				u, p, ok := r.BasicAuth()
-				if err := authn.Validate(u, p); !ok || err != nil {
-					rw.WriteHeader(http.StatusUnauthorized)
-
-					panic(http.StatusUnauthorized)
-				}
-
-				pc, err := db.GetCommunities(ctx)
-				if err != nil {
-					panic(err)
-				}
-
-				j, err := json.Marshal(pc)
-				if err != nil {
-					panic(err)
-				}
-
-				if _, err := fmt.Fprint(rw, string(j)); err != nil {
-					panic(err)
-				}
-
-				return
-			}
-
-			// Create ephermal community
-			password := r.URL.Query().Get("password")
-			if strings.TrimSpace(password) == "" {
-				panic(errMissingPassword)
-			}
-
-			if err := db.AddClientsToCommunity(ctx, community, password, *ephermalCommunities); err != nil {
-				if err == persisters.ErrWrongPassword || err == persisters.ErrEphermalCommunitiesDisabled {
-					rw.WriteHeader(http.StatusUnauthorized)
-
-					panic(http.StatusUnauthorized)
-				} else {
-					panic(err)
-				}
-			}
-
-			defer func() {
-				if err := db.RemoveClientFromCommunity(ctx, community); err != nil {
-					panic(err)
-				}
-			}()
-
-			conn, err := upgrader.Upgrade(rw, r, nil)
-			if err != nil {
-				panic(err)
-			}
-
-			defer func() {
-				connectionsLock.Lock()
-				delete(connections[community], raddr)
-				if len(connections[community]) <= 0 {
-					delete(connections, community)
-				}
-				connectionsLock.Unlock()
-
-				log.Println("Disconnected from client with address", raddr, "in community", community)
-
-				if err := conn.Close(); err != nil {
-					panic(err)
-				}
-			}()
-
-			connectionsLock.Lock()
-			if _, exists := connections[community]; !exists {
-				connections[community] = map[string]connection{}
-			}
-			connections[community][raddr] = connection{
-				conn:   conn,
-				closer: make(chan struct{}),
-			}
-			connectionsLock.Unlock()
-
-			log.Println("Connected to client with address", raddr, "in community", community)
-
-			if err := conn.SetReadDeadline(time.Now().Add(*heartbeat)); err != nil {
-				panic(err)
-			}
-			conn.SetPongHandler(func(string) error {
-				return conn.SetReadDeadline(time.Now().Add(*heartbeat))
-			})
-
-			pings := time.NewTicker(*heartbeat / 2)
-			defer pings.Stop()
-
-			errs := make(chan error)
-			go func() {
-				for {
-					messageType, p, err := conn.ReadMessage()
-					if err != nil {
-						if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure, websocket.CloseNoStatusReceived) {
-							errs <- err
-						}
-
-						errs <- nil
-
-						return
-					}
-
-					if *verbose {
-						log.Println("Received message with type", messageType, "from client with address", raddr, "in community", community)
-					}
-
-					if err := broker.PublishInput(ctx, brokers.Input{
-						Raddr:       raddr,
-						MessageType: messageType,
-						P:           p,
-					}, community); err != nil {
-						errs <- err
-
-						return
-					}
-				}
-			}()
-
-			inputs, closeInputs := broker.SubscribeToInputs(ctx, errs, community)
-			defer func() {
-				if err := closeInputs(); err != nil {
-					panic(err)
-				}
-			}()
-
-			for {
-				select {
-				case <-connections[community][raddr].closer:
-					return
-				case err := <-errs:
-					panic(err)
-				case input := <-inputs:
-					// Prevent sending message back to sender
-					if input.Raddr == raddr {
-						continue
-					}
-
-					if err := conn.WriteMessage(input.MessageType, input.P); err != nil {
-						panic(err)
-					}
-
-					if err := conn.SetWriteDeadline(time.Now().Add(*heartbeat)); err != nil {
-						panic(err)
-					}
-				case <-pings.C:
-					if *verbose {
-						log.Println("Sending ping to client with address", raddr, "in community", community)
-					}
-
-					if err := conn.SetWriteDeadline(time.Now().Add(*heartbeat)); err != nil {
-						panic(err)
-					}
-
-					if err := conn.WriteMessage(websocket.PingMessage, nil); err != nil {
-						panic(err)
-					}
-				}
-			}
-		case http.MethodPost:
-			if !managementAPIEnabled {
-				rw.WriteHeader(http.StatusNotImplemented)
-
-				panic(http.StatusNotImplemented)
-			}
-
-			// Create persistent community
-			u, p, ok := r.BasicAuth()
-			if err := authn.Validate(u, p); !ok || err != nil {
-				rw.WriteHeader(http.StatusUnauthorized)
-
-				panic(http.StatusUnauthorized)
-			}
-
-			password := r.URL.Query().Get("password")
-			if strings.TrimSpace(password) == "" {
-				panic(errMissingPassword)
-			}
-
-			community := r.URL.Query().Get("community")
-			if strings.TrimSpace(community) == "" {
-				panic(errMissingCommunity)
-			}
-
-			c, err := db.CreatePersistentCommunity(ctx, community, password)
-			if err != nil {
-				panic(err)
-			}
-
-			cc := persisters.Community{
-				ID:         c.ID,
-				Clients:    c.Clients,
-				Persistent: c.Persistent,
-			}
-
-			j, err := json.Marshal(cc)
-			if err != nil {
-				panic(err)
-			}
-
-			if _, err := fmt.Fprint(rw, string(j)); err != nil {
-				panic(err)
-			}
-
-			return
-		case http.MethodDelete:
-			if !managementAPIEnabled {
-				rw.WriteHeader(http.StatusNotImplemented)
-
-				panic(http.StatusNotImplemented)
-			}
-
-			// Delete persistent community
-			u, p, ok := r.BasicAuth()
-			if err := authn.Validate(u, p); !ok || err != nil {
-				rw.WriteHeader(http.StatusUnauthorized)
-
-				panic(http.StatusUnauthorized)
-			}
-
-			community := r.URL.Query().Get("community")
-			if strings.TrimSpace(community) == "" {
-				panic(errMissingCommunity)
-			}
-
-			if err := db.DeleteCommunity(ctx, community); err != nil {
-				if err == sql.ErrNoRows {
-					rw.WriteHeader(http.StatusNotFound)
-
-					panic(http.StatusNotFound)
-				} else {
-					panic(err)
-				}
-			}
-
-			if err := broker.PublishKick(ctx, brokers.Kick{
-				Community: community,
-			}); err != nil {
-				panic(err)
-			}
-
-			return
-		default:
-			rw.WriteHeader(http.StatusNotImplemented)
-
-			panic(http.StatusNotImplemented)
-		}
-	})
-
-	go func() {
-		for {
-			kick := <-kicks
-
-			c, ok := connections[kick.Community]
-			if !ok {
-				continue
-			}
-
-			for _, conn := range c {
-				close(conn.closer)
-			}
-		}
-	}()
-
-	go func() {
-		if err := srv.ListenAndServe(); err != nil {
-			if err == http.ErrServerClosed {
-				close(errs)
-
-				return
-			}
-
-			errs <- err
-
-			return
 		}
 	}()
 
 	log.Println("Listening on address", addr.String())
 
-	for err := range errs {
-		if err != nil {
-			panic(err)
-		}
+	if err := signaler.Wait(); err != nil {
+		panic(err)
 	}
 }
---
+++ pkg/wrtcsgl/wrtcsgl.go
@@ -0,0 +1,535 @@
+package wrtcsgl
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+
+	rediserr "github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+	"github.com/pojntfx/webrtcfd/internal/authn"
+	"github.com/pojntfx/webrtcfd/internal/authn/basic"
+	"github.com/pojntfx/webrtcfd/internal/authn/oidc"
+	"github.com/pojntfx/webrtcfd/internal/brokers"
+	"github.com/pojntfx/webrtcfd/internal/brokers/process"
+	"github.com/pojntfx/webrtcfd/internal/brokers/redis"
+	"github.com/pojntfx/webrtcfd/internal/persisters"
+	"github.com/pojntfx/webrtcfd/internal/persisters/memory"
+	"github.com/pojntfx/webrtcfd/internal/persisters/psql"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+)
+
+var (
+	errMissingCommunity = errors.New("missing community")
+	errMissingPassword  = errors.New("missing password")
+
+	upgrader = websocket.Upgrader{}
+)
+
+type connection struct {
+	conn   *websocket.Conn
+	closer chan struct{}
+}
+
+type SignalerConfig struct {
+	Heartbeat           time.Duration
+	Cleanup             bool
+	EphermalCommunities bool
+	APIUsername         string
+	APIPassword         string
+	OIDCIssuer          string
+	OIDCClientID        string
+	Verbose             bool
+
+	OnConnect    func(raddr string, community string)
+	OnDisconnect func(raddr string, community string, err interface{})
+}
+
+type Signaler struct {
+	laddr       string
+	postgresURL string
+	redisURL    string
+	config      *SignalerConfig
+	ctx         context.Context
+
+	errs            chan error
+	connectionsLock sync.Mutex
+	connections     map[string]map[string]connection
+	db              persisters.CommunitiesPersister
+	broker          brokers.CommunitiesBroker
+	srv             *http.Server
+	closeKicks      func() error
+}
+
+func NewSignaler(
+	laddr string,
+	dbURL string,
+	brokerURL string,
+	config *SignalerConfig,
+	ctx context.Context,
+) *Signaler {
+	if config == nil {
+		config = &SignalerConfig{}
+	}
+
+	return &Signaler{
+		laddr:       laddr,
+		postgresURL: dbURL,
+		redisURL:    brokerURL,
+		config:      config,
+		ctx:         ctx,
+
+		errs: make(chan error),
+	}
+}
+
+func (s *Signaler) Open() error {
+	if s.config.Verbose {
+		boil.DebugMode = true
+	}
+
+	addr, err := net.ResolveTCPAddr("tcp", s.laddr)
+	if err != nil {
+		return err
+	}
+
+	managementAPIEnabled := true
+	if (strings.TrimSpace(s.config.OIDCIssuer) == "" && strings.TrimSpace(s.config.OIDCClientID) == "") && strings.TrimSpace(s.config.APIPassword) == "" {
+		managementAPIEnabled = false
+
+		if s.config.Verbose {
+			log.Println("API password not set, disabling management API")
+		}
+	}
+
+	if strings.TrimSpace(s.postgresURL) == "" {
+		s.db = memory.NewCommunitiesPersister()
+	} else {
+		s.db = psql.NewCommunitiesPersister()
+	}
+
+	if err := s.db.Open(s.postgresURL); err != nil {
+		return err
+	}
+
+	if s.config.Cleanup {
+		if err := s.db.Cleanup(s.ctx); err != nil {
+			return err
+		}
+	}
+
+	if strings.TrimSpace(s.redisURL) == "" {
+		s.broker = process.NewCommunitiesBroker()
+	} else {
+		s.broker = redis.NewCommunitiesBroker()
+	}
+
+	if err := s.broker.Open(s.ctx, s.redisURL); err != nil {
+		return err
+	}
+
+	var authn authn.Authn
+	if strings.TrimSpace(s.config.OIDCIssuer) == "" && strings.TrimSpace(s.config.OIDCClientID) == "" {
+		authn = basic.NewAuthn(s.config.APIUsername, s.config.APIPassword)
+	} else {
+		authn = oidc.NewAuthn(s.config.OIDCIssuer, s.config.OIDCClientID)
+	}
+
+	if err := authn.Open(s.ctx); err != nil {
+		return err
+	}
+
+	s.srv = &http.Server{Addr: addr.String()}
+
+	s.connections = map[string]map[string]connection{}
+
+	kicks, closeKicks := s.broker.SubscribeToKicks(s.ctx, s.errs)
+	s.closeKicks = closeKicks
+
+	s.srv.Handler = http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		raddr := uuid.New().String()
+
+		defer func() {
+			err := recover()
+
+			switch err {
+			case nil:
+				if s.config.Verbose {
+					log.Println("closed connection for client with address", raddr)
+				}
+			case http.StatusUnauthorized:
+				fallthrough
+			case http.StatusNotImplemented:
+				if s.config.Verbose {
+					log.Println("closed connection for client with address", raddr+":", err)
+				}
+			case http.StatusNotFound:
+				if s.config.Verbose {
+					log.Println("closed connection for client with address", raddr+":", err)
+				}
+			default:
+				rw.WriteHeader(http.StatusInternalServerError)
+
+				if s.config.Verbose {
+					log.Println("closed connection for client with address", raddr+":", err)
+				}
+			}
+		}()
+
+		switch r.Method {
+		case http.MethodGet:
+			community := r.URL.Query().Get("community")
+			if strings.TrimSpace(community) == "" {
+				if !managementAPIEnabled {
+					rw.WriteHeader(http.StatusNotImplemented)
+
+					panic(http.StatusNotImplemented)
+				}
+
+				// List communities
+				u, p, ok := r.BasicAuth()
+				if err := authn.Validate(u, p); !ok || err != nil {
+					rw.WriteHeader(http.StatusUnauthorized)
+
+					panic(http.StatusUnauthorized)
+				}
+
+				pc, err := s.db.GetCommunities(s.ctx)
+				if err != nil {
+					panic(err)
+				}
+
+				j, err := json.Marshal(pc)
+				if err != nil {
+					panic(err)
+				}
+
+				if _, err := fmt.Fprint(rw, string(j)); err != nil {
+					panic(err)
+				}
+
+				return
+			}
+
+			// Create ephermal community
+			password := r.URL.Query().Get("password")
+			if strings.TrimSpace(password) == "" {
+				panic(errMissingPassword)
+			}
+
+			if err := s.db.AddClientsToCommunity(s.ctx, community, password, s.config.EphermalCommunities); err != nil {
+				if err == persisters.ErrWrongPassword || err == persisters.ErrEphermalCommunitiesDisabled {
+					rw.WriteHeader(http.StatusUnauthorized)
+
+					panic(http.StatusUnauthorized)
+				} else {
+					panic(err)
+				}
+			}
+
+			defer func() {
+				if err := s.db.RemoveClientFromCommunity(s.ctx, community); err != nil {
+					panic(err)
+				}
+			}()
+
+			conn, err := upgrader.Upgrade(rw, r, nil)
+			if err != nil {
+				panic(err)
+			}
+
+			defer func() {
+				s.connectionsLock.Lock()
+				delete(s.connections[community], raddr)
+				if len(s.connections[community]) <= 0 {
+					delete(s.connections, community)
+				}
+				s.connectionsLock.Unlock()
+
+				if s.config.Verbose {
+					log.Println("Disconnected from client with address", raddr, "in community", community)
+				}
+
+				if s.config.OnDisconnect != nil {
+					s.config.OnDisconnect(raddr, community, err)
+				}
+
+				if err := conn.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			s.connectionsLock.Lock()
+			if _, exists := s.connections[community]; !exists {
+				s.connections[community] = map[string]connection{}
+			}
+			s.connections[community][raddr] = connection{
+				conn:   conn,
+				closer: make(chan struct{}),
+			}
+			s.connectionsLock.Unlock()
+
+			if s.config.Verbose {
+				log.Println("Connected to client with address", raddr, "in community", community)
+			}
+
+			if s.config.OnConnect != nil {
+				s.config.OnConnect(raddr, community)
+			}
+
+			if err := conn.SetReadDeadline(time.Now().Add(s.config.Heartbeat)); err != nil {
+				panic(err)
+			}
+			conn.SetPongHandler(func(string) error {
+				return conn.SetReadDeadline(time.Now().Add(s.config.Heartbeat))
+			})
+
+			pings := time.NewTicker(s.config.Heartbeat / 2)
+			defer pings.Stop()
+
+			errs := make(chan error)
+			go func() {
+				for {
+					messageType, p, err := conn.ReadMessage()
+					if err != nil {
+						if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure, websocket.CloseNoStatusReceived) {
+							errs <- err
+						}
+
+						errs <- nil
+
+						return
+					}
+
+					if s.config.Verbose {
+						log.Println("Received message with type", messageType, "from client with address", raddr, "in community", community)
+					}
+
+					if err := s.broker.PublishInput(s.ctx, brokers.Input{
+						Raddr:       raddr,
+						MessageType: messageType,
+						P:           p,
+					}, community); err != nil {
+						errs <- err
+
+						return
+					}
+				}
+			}()
+
+			inputs, closeInputs := s.broker.SubscribeToInputs(s.ctx, errs, community)
+			defer func() {
+				if err := closeInputs(); err != nil {
+					panic(err)
+				}
+			}()
+
+			for {
+				select {
+				case <-s.connections[community][raddr].closer:
+					return
+				case err := <-errs:
+					panic(err)
+				case input := <-inputs:
+					// Prevent sending message back to sender
+					if input.Raddr == raddr {
+						continue
+					}
+
+					if err := conn.WriteMessage(input.MessageType, input.P); err != nil {
+						panic(err)
+					}
+
+					if err := conn.SetWriteDeadline(time.Now().Add(s.config.Heartbeat)); err != nil {
+						panic(err)
+					}
+				case <-pings.C:
+					if s.config.Verbose {
+						log.Println("Sending ping to client with address", raddr, "in community", community)
+					}
+
+					if err := conn.SetWriteDeadline(time.Now().Add(s.config.Heartbeat)); err != nil {
+						panic(err)
+					}
+
+					if err := conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+						panic(err)
+					}
+				}
+			}
+		case http.MethodPost:
+			if !managementAPIEnabled {
+				rw.WriteHeader(http.StatusNotImplemented)
+
+				panic(http.StatusNotImplemented)
+			}
+
+			// Create persistent community
+			u, p, ok := r.BasicAuth()
+			if err := authn.Validate(u, p); !ok || err != nil {
+				rw.WriteHeader(http.StatusUnauthorized)
+
+				panic(http.StatusUnauthorized)
+			}
+
+			password := r.URL.Query().Get("password")
+			if strings.TrimSpace(password) == "" {
+				panic(errMissingPassword)
+			}
+
+			community := r.URL.Query().Get("community")
+			if strings.TrimSpace(community) == "" {
+				panic(errMissingCommunity)
+			}
+
+			c, err := s.db.CreatePersistentCommunity(s.ctx, community, password)
+			if err != nil {
+				panic(err)
+			}
+
+			cc := persisters.Community{
+				ID:         c.ID,
+				Clients:    c.Clients,
+				Persistent: c.Persistent,
+			}
+
+			j, err := json.Marshal(cc)
+			if err != nil {
+				panic(err)
+			}
+
+			if _, err := fmt.Fprint(rw, string(j)); err != nil {
+				panic(err)
+			}
+
+			return
+		case http.MethodDelete:
+			if !managementAPIEnabled {
+				rw.WriteHeader(http.StatusNotImplemented)
+
+				panic(http.StatusNotImplemented)
+			}
+
+			// Delete persistent community
+			u, p, ok := r.BasicAuth()
+			if err := authn.Validate(u, p); !ok || err != nil {
+				rw.WriteHeader(http.StatusUnauthorized)
+
+				panic(http.StatusUnauthorized)
+			}
+
+			community := r.URL.Query().Get("community")
+			if strings.TrimSpace(community) == "" {
+				panic(errMissingCommunity)
+			}
+
+			if err := s.db.DeleteCommunity(s.ctx, community); err != nil {
+				if err == sql.ErrNoRows {
+					rw.WriteHeader(http.StatusNotFound)
+
+					panic(http.StatusNotFound)
+				} else {
+					panic(err)
+				}
+			}
+
+			if err := s.broker.PublishKick(s.ctx, brokers.Kick{
+				Community: community,
+			}); err != nil {
+				panic(err)
+			}
+
+			return
+		default:
+			rw.WriteHeader(http.StatusNotImplemented)
+
+			panic(http.StatusNotImplemented)
+		}
+	})
+
+	go func() {
+		for {
+			kick := <-kicks
+
+			s.connectionsLock.Lock()
+			c, ok := s.connections[kick.Community]
+			if !ok {
+				s.connectionsLock.Unlock()
+
+				continue
+			}
+			s.connectionsLock.Unlock()
+
+			for _, conn := range c {
+				close(conn.closer)
+			}
+		}
+	}()
+
+	go func() {
+		if err := s.srv.ListenAndServe(); err != nil {
+			if err == http.ErrServerClosed {
+				close(s.errs)
+
+				return
+			}
+
+			s.errs <- err
+
+			return
+		}
+	}()
+
+	return nil
+}
+
+func (s *Signaler) Close() error {
+	s.connectionsLock.Lock()
+	defer s.connectionsLock.Unlock()
+	for c := range s.connections {
+		for range s.connections[c] {
+			if err := s.db.RemoveClientFromCommunity(s.ctx, c); err != nil {
+				return err
+			}
+		}
+	}
+
+	if err := s.closeKicks(); err != nil {
+		if err != context.Canceled {
+			return err
+		}
+	}
+
+	if err := s.broker.Close(); err != nil {
+		if err != context.Canceled && err != rediserr.ErrClosed {
+			return err
+		}
+	}
+
+	if err := s.srv.Shutdown(s.ctx); err != nil {
+		if err != context.Canceled {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Signaler) Wait() error {
+	for err := range s.errs {
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
