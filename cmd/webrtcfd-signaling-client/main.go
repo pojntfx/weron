d1778ee2fd3e0b6fd4db03833be98a6be3382650
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-03-31 23:36:49 +0200

    refactor: Decompose client API

--- cmd/webrtcfd-signaling-client/main.go
+++ cmd/webrtcfd-signaling-client/main.go
@@ -7,16 +7,10 @@ import (
 	"log"
 	"net/url"
 	"strings"
-	"sync"
 	"time"
 
-	"encoding/json"
-
-	"github.com/google/uuid"
-	"github.com/gorilla/websocket"
 	"github.com/pion/webrtc/v3"
-	websocketapi "github.com/pojntfx/webrtcfd/internal/api/websocket"
-	"github.com/pojntfx/webrtcfd/internal/encryption"
+	"github.com/pojntfx/webrtcfd/pkg/wrtcconn"
 )
 
 var (
@@ -45,11 +39,13 @@ func main() {
 	community := flag.String("community", "", "ID of community to join")
 	password := flag.String("password", "", "Password for community")
 	key := flag.String("key", "", "Encryption key for community")
-	stun := flag.String("stun", "stun:stun.l.google.com:19302", "Comma-seperated list of STUN servers to use (in format stun:host:port)")
-	turn := flag.String("turn", "", "Comma-seperated list of TURN servers to use (in format username:credential@turn:host:port) (i.e. username:credential@turn:global.turn.twilio.com:3478?transport=tcp)")
+	ice := flag.String("ice", "stun:stun.l.google.com:19302", "Comma-seperated list of STUN servers (in format stun:host:port) and TURN servers to use (in format username:credential@turn:host:port) (i.e. username:credential@turn:global.turn.twilio.com:3478?transport=tcp)")
 	verbose := flag.Bool("verbose", false, "Enable verbose logging")
 
 	flag.Parse()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	if strings.TrimSpace(*community) == "" {
 		panic(errMissingCommunity)
@@ -75,601 +71,89 @@ func main() {
 	q.Set("password", *password)
 	u.RawQuery = q.Encode()
 
-	iceServers := []webrtc.ICEServer{}
+	adapter := wrtcconn.NewAdapter(
+		u.String(),
+		*key,
+		strings.Split(*ice, ","),
+		&wrtcconn.AdapterConfig{
+			Timeout: *timeout,
+			Verbose: *verbose,
+		},
+		ctx,
+	)
 
-	for _, stunServer := range strings.Split(*stun, ",") {
-		// Skip empty STUN server configs
-		if strings.TrimSpace(stunServer) == "" {
-			continue
-		}
-
-		iceServers = append(iceServers, webrtc.ICEServer{
-			URLs: []string{stunServer},
-		})
+	ids, err := adapter.Open()
+	if err != nil {
+		panic(err)
 	}
-
-	for _, turnServer := range strings.Split(*turn, ",") {
-		// Skip empty TURN server configs
-		if strings.TrimSpace(turnServer) == "" {
-			continue
+	defer func() {
+		if err := adapter.Close(); err != nil {
+			panic(err)
 		}
+	}()
 
-		addrParts := strings.Split(turnServer, "@")
-		if len(addrParts) < 2 {
-			panic(errInvalidTURNServerAddr)
+	id := ""
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case id = <-ids:
+				log.Println("Connected as", id)
+			}
 		}
-
-		authParts := strings.Split(addrParts[0], ":")
-		if len(addrParts) < 2 {
-			panic(errMissingTURNCredentials)
-		}
-
-		iceServers = append(iceServers, webrtc.ICEServer{
-			URLs:           []string{addrParts[1]},
-			Username:       authParts[0],
-			Credential:     authParts[1],
-			CredentialType: webrtc.ICECredentialTypePassword,
-		})
-	}
-
-	lines := make(chan []byte)
-	defer close(lines)
-
-	peers := map[string]*peer{}
-	var peerLock sync.Mutex
+	}()
 
 	for {
-		func() {
+		peer, conn, err := adapter.Accept()
+		if err != nil {
+			log.Println("Could not accept peer, continuing")
+
+			continue
+		}
+
+		go func() {
+			log.Println("Peer connected", peer)
 			defer func() {
-				if err := recover(); err != nil {
-					log.Println("closed connection to signaler with address", *raddr+":", err, "(wrong username or password?)")
-				}
-
-				log.Println("Reconnecting to signaler with address", *raddr, "in", *timeout)
-
-				time.Sleep(*timeout)
+				log.Println("Peer disconnected", peer)
 			}()
 
-			ctx, cancel := context.WithTimeout(context.Background(), *timeout)
-			defer cancel()
-
-			conn, _, err := websocket.DefaultDialer.DialContext(ctx, u.String(), nil)
-			if err != nil {
-				panic(err)
-			}
-
-			defer func() {
-				log.Println("Disconnected from signaler with address", *raddr)
-
-				if err := conn.Close(); err != nil {
-					panic(err)
-				}
-
-				for _, peer := range peers {
-					if err := peer.conn.Close(); err != nil {
-						panic(err)
-					}
-
-					close(peer.candidates)
-				}
-			}()
-
-			log.Println("Connected to signaler with address", *raddr)
-
-			inputs := make(chan []byte)
-			errs := make(chan error)
-			go func() {
-				defer func() {
-					close(inputs)
-					close(errs)
-				}()
-
-				for {
-					_, p, err := conn.ReadMessage()
-					if err != nil {
-						errs <- err
-
+			for {
+				buf := make([]byte, 1024)
+				n, err := conn.Read(buf)
+				if err != nil {
+					if err := conn.Close(); err != nil {
 						return
 					}
-
-					inputs <- p
-				}
-			}()
-
-			id := uuid.New().String()
-
-			go func() {
-				p, err := json.Marshal(websocketapi.NewIntroduction(id))
-				if err != nil {
-					errs <- err
 
 					return
 				}
 
-				lines <- p
+				log.Println("Received message from peer", peer, "with length", n, string(buf))
+			}
+		}()
 
-				log.Println("Introduced to signaler with address", *raddr, "and ID", id)
+		go func() {
+			defer func() {
+				log.Println("Peer disconnected", peer)
 			}()
 
+			ticker := time.NewTicker(time.Second)
 			for {
 				select {
-				case err := <-errs:
-					panic(err)
-				case input := <-inputs:
-					input, err = encryption.Decrypt(input, []byte(*key))
+				case <-ctx.Done():
+					return
+				case <-ticker.C:
+					n, err := conn.Write([]byte("Hello from " + id + "!"))
 					if err != nil {
-						if *verbose {
-							log.Println("Could not decrypt message with length", len(input), "for signaler with address", conn.RemoteAddr(), "in community", *community+", skipping")
+						if err := conn.Close(); err != nil {
+							return
 						}
 
-						continue
+						return
 					}
 
-					if *verbose {
-						log.Println("Received message with length", len(input), "from signaler with address", conn.RemoteAddr(), "in community", *community)
-					}
-
-					var message websocketapi.Message
-					if err := json.Unmarshal(input, &message); err != nil {
-						if *verbose {
-							log.Println("Could not unmarshal message for signaler with address", conn.RemoteAddr(), "in community", *community+", skipping")
-						}
-
-						continue
-					}
-
-					switch message.Type {
-					case websocketapi.TypeIntroduction:
-						var introduction websocketapi.Introduction
-						if err := json.Unmarshal(input, &introduction); err != nil {
-							if *verbose {
-								log.Println("Could not unmarshal introduction for signaler with address", conn.RemoteAddr(), "in community", *community+", skipping")
-							}
-
-							continue
-						}
-
-						if *verbose {
-							log.Println("Received introduction", introduction, "from signaler with address", conn.RemoteAddr(), "in community", *community)
-						}
-
-						c, err := webrtc.NewPeerConnection(webrtc.Configuration{
-							ICEServers: iceServers,
-						})
-						if err != nil {
-							panic(err)
-						}
-
-						c.OnConnectionStateChange(func(pcs webrtc.PeerConnectionState) {
-							if pcs == webrtc.PeerConnectionStateDisconnected {
-								log.Println("Disconnected from peer", introduction.From)
-
-								peerLock.Lock()
-								defer peerLock.Unlock()
-
-								c, ok := peers[introduction.From]
-
-								if !ok {
-									if *verbose {
-										log.Println("Could not find connection for peer", introduction.From, ", skipping")
-									}
-
-									return
-								}
-
-								if err := c.channel.Close(); err != nil {
-									panic(err)
-								}
-
-								if err := c.conn.Close(); err != nil {
-									panic(err)
-								}
-
-								close(c.candidates)
-
-								delete(peers, introduction.From)
-							}
-						})
-
-						c.OnICECandidate(func(i *webrtc.ICECandidate) {
-							if i != nil {
-								if *verbose {
-									log.Println("Created ICE candidate", i, "for signaler with address", conn.RemoteAddr(), "in community", *community)
-								}
-
-								p, err := json.Marshal(websocketapi.NewCandidate(id, introduction.From, []byte(i.ToJSON().Candidate)))
-								if err != nil {
-									panic(err)
-								}
-
-								go func() {
-									lines <- p
-
-									if *verbose {
-										log.Println("Sent candidate to signaler with address", *raddr, "and ID", id, "to client", introduction.From)
-									}
-								}()
-							}
-						})
-
-						dc, err := c.CreateDataChannel(dataChannelName, nil)
-						if err != nil {
-							panic(err)
-						}
-						closed := false
-
-						if *verbose {
-							log.Println("Created data channel using signaler with address", conn.RemoteAddr(), "in community", *community)
-						}
-
-						dc.OnOpen(func() {
-							log.Println("Connected to peer", introduction.From)
-
-							for {
-								time.Sleep(time.Second)
-
-								if closed {
-									return
-								}
-
-								if err := dc.Send([]byte("Hello from peer " + id)); err != nil {
-									panic(err)
-								}
-
-								if *verbose {
-									log.Println("Sent message to peer", introduction.From)
-								}
-							}
-						})
-
-						dc.OnClose(func() {
-							closed = true
-						})
-
-						dc.OnMessage(func(msg webrtc.DataChannelMessage) {
-							log.Println("Received message from", introduction.From, string(msg.Data))
-						})
-
-						o, err := c.CreateOffer(nil)
-						if err != nil {
-							panic(err)
-						}
-
-						if err := c.SetLocalDescription(o); err != nil {
-							panic(err)
-						}
-
-						oj, err := json.Marshal(o)
-						if err != nil {
-							panic(err)
-						}
-
-						p, err := json.Marshal(websocketapi.NewOffer(id, introduction.From, oj))
-						if err != nil {
-							panic(err)
-						}
-
-						peerLock.Lock()
-						peers[introduction.From] = &peer{c, make(chan webrtc.ICECandidateInit), dc}
-						peerLock.Unlock()
-
-						go func() {
-							lines <- p
-
-							if *verbose {
-								log.Println("Sent offer to signaler with address", *raddr, "and ID", id, "to client", introduction.From)
-							}
-						}()
-					case websocketapi.TypeOffer:
-						var offer websocketapi.Exchange
-						if err := json.Unmarshal(input, &offer); err != nil {
-							if *verbose {
-								log.Println("Could not unmarshal offer for signaler with address", conn.RemoteAddr(), "in community", *community+", skipping")
-							}
-
-							continue
-						}
-
-						if *verbose {
-							log.Println("Received offer", offer, "from signaler with address", conn.RemoteAddr(), "in community", *community)
-						}
-
-						if offer.To != id {
-							if *verbose {
-								log.Println("Discarding offer", offer, "from signaler with address", conn.RemoteAddr(), "in community", *community, "because it is not intended for this client")
-							}
-
-							continue
-						}
-
-						c, err := webrtc.NewPeerConnection(webrtc.Configuration{
-							ICEServers: iceServers,
-						})
-						if err != nil {
-							panic(err)
-						}
-
-						c.OnConnectionStateChange(func(pcs webrtc.PeerConnectionState) {
-							if pcs == webrtc.PeerConnectionStateDisconnected {
-								log.Println("Disconnected from peer", offer.From)
-
-								peerLock.Lock()
-								defer peerLock.Unlock()
-
-								c, ok := peers[offer.From]
-
-								if !ok {
-									if *verbose {
-										log.Println("Could not find connection for peer", offer.From, ", skipping")
-									}
-
-									return
-								}
-
-								if err := c.channel.Close(); err != nil {
-									panic(err)
-								}
-
-								if err := c.conn.Close(); err != nil {
-									panic(err)
-								}
-
-								close(c.candidates)
-
-								delete(peers, offer.From)
-							}
-						})
-
-						c.OnICECandidate(func(i *webrtc.ICECandidate) {
-							if i != nil {
-								if *verbose {
-									log.Println("Created ICE candidate", i, "for signaler with address", conn.RemoteAddr(), "in community", *community)
-								}
-
-								p, err := json.Marshal(websocketapi.NewCandidate(id, offer.From, []byte(i.ToJSON().Candidate)))
-								if err != nil {
-									panic(err)
-								}
-
-								go func() {
-									lines <- p
-
-									if *verbose {
-										log.Println("Sent candidate to signaler with address", *raddr, "and ID", id, "to client", offer.From)
-									}
-								}()
-							}
-						})
-
-						c.OnDataChannel(func(dc *webrtc.DataChannel) {
-							closed := false
-
-							dc.OnOpen(func() {
-								log.Println("Connected to peer", offer.From)
-
-								peerLock.Lock()
-								peers[offer.From].channel = dc
-								peerLock.Unlock()
-
-								for {
-									time.Sleep(time.Second)
-
-									if closed {
-										return
-									}
-
-									if err := dc.Send([]byte("Hello from peer " + id)); err != nil {
-										panic(err)
-									}
-
-									if *verbose {
-										log.Println("Sent message to peer", offer.From)
-									}
-								}
-							})
-
-							dc.OnClose(func() {
-								closed = true
-							})
-
-							dc.OnMessage(func(msg webrtc.DataChannelMessage) {
-								log.Println("Received message from", offer.From, string(msg.Data))
-							})
-						})
-
-						var sdp webrtc.SessionDescription
-						if err := json.Unmarshal(offer.Payload, &sdp); err != nil {
-							if *verbose {
-								log.Println("Could not unmarshal SDP for signaler with address", conn.RemoteAddr(), "in community", *community+", skipping")
-							}
-
-							continue
-						}
-
-						if err := c.SetRemoteDescription(sdp); err != nil {
-							panic(err)
-						}
-
-						a, err := c.CreateAnswer(nil)
-						if err != nil {
-							panic(err)
-						}
-
-						if err := c.SetLocalDescription(a); err != nil {
-							panic(err)
-						}
-
-						aj, err := json.Marshal(a)
-						if err != nil {
-							panic(err)
-						}
-
-						p, err := json.Marshal(websocketapi.NewAnswer(id, offer.From, aj))
-						if err != nil {
-							panic(err)
-						}
-
-						peerLock.Lock()
-
-						candidates := make(chan webrtc.ICECandidateInit)
-						peers[offer.From] = &peer{c, candidates, nil}
-
-						peerLock.Unlock()
-
-						go func() {
-							for candidate := range candidates {
-								if err := c.AddICECandidate(candidate); err != nil {
-									errs <- err
-
-									return
-								}
-
-								if *verbose {
-									log.Println("Added ICE candidate from signaler with address", *raddr, "and ID", id, "from client", offer.From)
-								}
-							}
-						}()
-
-						go func() {
-							lines <- p
-
-							if *verbose {
-								log.Println("Sent answer to signaler with address", *raddr, "and ID", id, "to client", offer.From)
-							}
-						}()
-					case websocketapi.TypeCandidate:
-						var candidate websocketapi.Exchange
-						if err := json.Unmarshal(input, &candidate); err != nil {
-							if *verbose {
-								log.Println("Could not unmarshal candidate for signaler with address", conn.RemoteAddr(), "in community", *community+", skipping")
-							}
-
-							continue
-						}
-
-						if *verbose {
-							log.Println("Received candidate", candidate, "from signaler with address", conn.RemoteAddr(), "in community", *community)
-						}
-
-						if candidate.To != id {
-							if *verbose {
-								log.Println("Discarding candidate", candidate, "from signaler with address", conn.RemoteAddr(), "in community", *community, "because it is not intended for this client")
-							}
-
-							continue
-						}
-
-						peerLock.Lock()
-						c, ok := peers[candidate.From]
-
-						if !ok {
-							if *verbose {
-								log.Println("Could not find connection for peer", candidate.From, ", skipping")
-							}
-
-							peerLock.Unlock()
-
-							continue
-						}
-
-						go func() {
-							defer func() {
-								if err := recover(); err != nil {
-									if *verbose {
-										log.Println("Gathering candidates has stopped, skipping candidate")
-									}
-								}
-							}()
-
-							c.candidates <- webrtc.ICECandidateInit{Candidate: string(candidate.Payload)}
-						}()
-
-						peerLock.Unlock()
-					case websocketapi.TypeAnswer:
-						var answer websocketapi.Exchange
-						if err := json.Unmarshal(input, &answer); err != nil {
-							if *verbose {
-								log.Println("Could not unmarshal answer for signaler with address", conn.RemoteAddr(), "in community", *community+", skipping")
-							}
-
-							continue
-						}
-
-						if *verbose {
-							log.Println("Received answer", answer, "from signaler with address", conn.RemoteAddr(), "in community", *community)
-						}
-
-						if answer.To != id {
-							if *verbose {
-								log.Println("Discarding answer", answer, "from signaler with address", conn.RemoteAddr(), "in community", *community, "because it is not intended for this client")
-							}
-
-							continue
-						}
-
-						peerLock.Lock()
-						c, ok := peers[answer.From]
-						peerLock.Unlock()
-
-						if !ok {
-							if *verbose {
-								log.Println("Could not find connection for peer", answer.From, ", skipping")
-							}
-
-							continue
-						}
-
-						var sdp webrtc.SessionDescription
-						if err := json.Unmarshal(answer.Payload, &sdp); err != nil {
-							if *verbose {
-								log.Println("Could not unmarshal SDP for signaler with address", conn.RemoteAddr(), "in community", *community+", skipping")
-							}
-
-							continue
-						}
-
-						if err := c.conn.SetRemoteDescription(sdp); err != nil {
-							panic(err)
-						}
-
-						go func() {
-							for candidate := range c.candidates {
-								if err := c.conn.AddICECandidate(candidate); err != nil {
-									errs <- err
-
-									return
-								}
-
-								if *verbose {
-									log.Println("Added ICE candidate from signaler with address", *raddr, "and ID", id, "from client", answer.From)
-								}
-							}
-						}()
-
-						if *verbose {
-							log.Println("Added answer from signaler with address", *raddr, "and ID", id, "from client", answer.From)
-						}
-					default:
-						if *verbose {
-							log.Println("Got message with unknown type", message.Type, "for signaler with address", conn.RemoteAddr(), "in community", *community+", skipping")
-						}
-
-						continue
-					}
-
-				case line := <-lines:
-					line, err = encryption.Encrypt(line, []byte(*key))
-					if err != nil {
-						panic(err)
-					}
-
-					if *verbose {
-						log.Println("Sending message with length", len(line), "to signaler with address", conn.RemoteAddr(), "in community", *community)
-					}
-
-					if err := conn.WriteMessage(websocket.TextMessage, line); err != nil {
-						panic(err)
-					}
+					log.Println("Sent message to peer", peer, "with length", n)
 				}
 			}
 		}()
