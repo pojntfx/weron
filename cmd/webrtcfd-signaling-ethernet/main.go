cef7fea789ee6124c901ce2ba7b17130cc3e2a3e
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-04-03 18:43:30 +0200

    feat: Support non-Linux platforms for `wrtceth`

--- cmd/webrtcfd-signaling-ethernet/main.go
+++ cmd/webrtcfd-signaling-ethernet/main.go
@@ -30,8 +30,8 @@ func main() {
 	password := flag.String("password", "", "Password for community")
 	key := flag.String("key", "", "Encryption key for community")
 	ice := flag.String("ice", "stun:stun.l.google.com:19302", "Comma-seperated list of STUN servers (in format stun:host:port) and TURN servers to use (in format username:credential@turn:host:port) (i.e. username:credential@turn:global.turn.twilio.com:3478?transport=tcp)")
-	dev := flag.String("dev", "", "Name to give to the TAP device (i.e. weron0) (default is auto-generated)")
-	mac := flag.String("mac", "", "MAC address to give to the TAP device (i.e. 3a:f8:de:7b:ef:52) (default is auto-generated)")
+	dev := flag.String("dev", "", "Name to give to the TAP device (i.e. weron0) (default is auto-generated; only supported on Linux, macOS and Windows)")
+	mac := flag.String("mac", "", "MAC address to give to the TAP device (i.e. 3a:f8:de:7b:ef:52) (default is auto-generated; only supported on Linux)")
 	verbose := flag.Bool("verbose", false, "Enable verbose logging")
 
 	flag.Parse()
---
+++ pkg/wrtceth/netns_darwin.go
@@ -0,0 +1,30 @@
+package wrtceth
+
+import (
+	"net"
+
+	"github.com/songgao/water"
+)
+
+func getPlatformSpecificParams(name string) water.PlatformSpecificParams {
+	return water.PlatformSpecificParams{
+		Name: name,
+	}
+}
+
+func setMACAddress(linkName string, hwaddr string) (string, error) {
+	return hwaddr, nil
+}
+
+func getMTU(linkName string) (int, error) {
+	iface, err := net.InterfaceByName(linkName)
+	if err != nil {
+		return -1, err
+	}
+
+	return iface.MTU, nil
+}
+
+func setLinkUp(linkName string) error {
+	return nil
+}
