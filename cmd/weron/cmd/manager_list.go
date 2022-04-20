a2277c49930a8acef95dbb1ee9197d12499f0444
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-04-20 21:32:07 +0200

    feat: Add `create` command to manager

--- cmd/weron/cmd/manager_list.go
+++ cmd/weron/cmd/manager_list.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/pojntfx/webrtcfd/pkg/wrtcmgr"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
@@ -27,25 +28,7 @@ var managerListCmd = &cobra.Command{
 	Use:     "list",
 	Aliases: []string{"lis", "l", "ls"},
 	Short:   "List persistent and ephermal communities",
-	PreRunE: func(cmd *cobra.Command, args []string) error {
-		if u := os.Getenv("API_USERNAME"); u != "" {
-			if viper.GetBool(verboseFlag) {
-				log.Println("Using username from API_USERNAME env variable")
-			}
-
-			viper.Set(apiUsernameFlag, u)
-		}
-
-		if u := os.Getenv("API_PASSWORD"); u != "" {
-			if viper.GetBool(verboseFlag) {
-				log.Println("Using password from API_PASSWORD env variable")
-			}
-
-			viper.Set(apiPasswordFlag, u)
-		}
-
-		return viper.BindPFlags(cmd.PersistentFlags())
-	},
+	PreRunE: validateRemoteFlags,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if err := viper.BindPFlags(cmd.PersistentFlags()); err != nil {
 			return err
@@ -92,11 +75,35 @@ var managerListCmd = &cobra.Command{
 }
 
 func init() {
-	managerListCmd.PersistentFlags().String(apiUsernameFlag, "admin", "Username for the management API (can also be set using the API_USERNAME env variable). Ignored if any of the OIDC parameters are set.")
-	managerListCmd.PersistentFlags().String(apiPasswordFlag, "", "Password for the management API (can also be set using the API_PASSWORD env variable). Ignored if any of the OIDC parameters are set.")
-	managerListCmd.PersistentFlags().String(raddrFlag, "https://webrtcfd-production.up.railway.app/", "Remote address")
+	addRemoteFlags(managerListCmd.PersistentFlags())
 
 	viper.AutomaticEnv()
 
 	managerCmd.AddCommand(managerListCmd)
+}
+
+func addRemoteFlags(f *pflag.FlagSet) {
+	f.String(apiUsernameFlag, "admin", "Username for the management API (can also be set using the API_USERNAME env variable). Ignored if any of the OIDC parameters are set.")
+	f.String(apiPasswordFlag, "", "Password for the management API (can also be set using the API_PASSWORD env variable). Ignored if any of the OIDC parameters are set.")
+	f.String(raddrFlag, "https://webrtcfd.herokuapp.com/", "Remote address")
+}
+
+func validateRemoteFlags(cmd *cobra.Command, args []string) error {
+	if u := os.Getenv("API_USERNAME"); u != "" {
+		if viper.GetBool(verboseFlag) {
+			log.Println("Using username from API_USERNAME env variable")
+		}
+
+		viper.Set(apiUsernameFlag, u)
+	}
+
+	if u := os.Getenv("API_PASSWORD"); u != "" {
+		if viper.GetBool(verboseFlag) {
+			log.Println("Using password from API_PASSWORD env variable")
+		}
+
+		viper.Set(apiPasswordFlag, u)
+	}
+
+	return viper.BindPFlags(cmd.PersistentFlags())
 }
